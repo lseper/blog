3f6938b82e83857544b2ab0bef2e7f7712f87072
Zaverose <seperliam@gmail.com>
2023-03-22 22:52:18 -0500

    initial

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import "../styles/index.css";
+
+import { Roboto } from "@next/font/google";
+
+const roboto = Roboto({
+	subsets: ["latin"],
+	weight: ["300", "400", "500", "700"],
+});
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
---
+++ theme/theme.ts
@@ -0,0 +1,31 @@
+import { createTheme } from "@mui/material/styles";
+// ugggggghhjghdjhgfjsdfhjgk
+// declare module "@mui/material/styles" {
+// 	interface Theme {
+// 		custom: {
+// 			twitter: string;
+// 			github: string;
+// 			linkedin: string;
+// 		};
+// 	}
+// 	// allow configuration using `createTheme`
+// 	interface ThemeOptions {
+// 		palette: {
+// 			custom?: {
+// 				twitter?: string;
+// 				github?: string;
+// 				linkedin?: string;
+// 			};
+// 		};
+// 	}
+// }
+
+// const theme = createTheme({
+// 	palette: {
+// 		custom: {
+// 			twitter: "#00acee",
+// 			github: "#6e5494",
+// 			linkedin: "#0077b5",
+// 		},
+// 	},
+// });
